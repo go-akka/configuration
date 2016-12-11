36889a00a06116a668388a782201fbd44a452178
Zeal <zhengxujin@Zeals-MBP.lan>
2016-12-11 22:05:49 +0800

    add more whitespace char sets

--- hocon/tokenizer.go
+++ hocon/tokenizer.go
@@ -528,9 +528,36 @@ func (p *HoconTokenizer) isValue() bool {
 	return false
 }
 
+/*
+SPACE (\u0020)
+NO-BREAK SPACE (\u00A0)
+OGHAM SPACE MARK (\u1680)
+EN QUAD (\u2000)
+EM QUAD (\u2001)
+EN SPACE (\u2002)
+EM SPACE (\u2003)
+THREE-PER-EM SPACE (\u2004)
+FOUR-PER-EM SPACE (\u2005)
+SIX-PER-EM SPACE (\u2006)
+FIGURE SPACE (\u2007)
+PUNCTUATION SPACE (\u2008)
+THIN SPACE (\u2009)
+HAIR SPACE (\u200A)
+NARROW NO-BREAK SPACE (\u202F)
+MEDIUM MATHEMATICAL SPACE (\u205F)
+and IDEOGRAPHIC SPACE (\u3000)
+Byte Order Mark (\uFEFF)
+*/
 func isWhitespace(c byte) bool {
 	str := string(c)
-	if str == " " || str == "\r" || str == "\n" || str == "\t" || str == "\u00A0" || str == "\u2007" || str == "\u202F" || str == "\uFEFF" {
+
+	switch str {
+	case " ", "\t", "\n", "\u000B", "\u000C",
+		"\u000D", "\u00A0", "\u1680", "\u2000",
+		"\u2001", "\u2002", "\u2003", "\u2004",
+		"\u2005", "\u2006", "\u2007", "\u2008",
+		"\u2009", "\u200A", "\u202F", "\u205F",
+		"\u2060", "\u3000", "\uFEFF":
 		return true
 	}
 	return false
